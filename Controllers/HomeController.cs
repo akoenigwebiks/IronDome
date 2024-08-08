3074d343e672be8beed64c317f9a5a42d297cf5c
Darkmift <darkmift@gmail.com>
2024-08-08 03:56:50 +0300

    removed unused files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,16 +8,6 @@ namespace IronDome.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         public IActionResult Privacy()
         {
             return View();
